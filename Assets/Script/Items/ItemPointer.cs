6a7fb1862828ffa11611584a3c5553196f10a96e
ifairuss <ifairuss2403artem@gmail.com>
2025-01-07 01:31:17 +0100

    Feature/Add-Animation-Marker

--- Assets/Script/Items/ItemPointer.cs
+++ Assets/Script/Items/ItemPointer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ItemPointer : MonoBehaviour
@@ -9,6 +10,7 @@ public class ItemPointer : MonoBehaviour
     {
         _player = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>();
         _gameObject = GetComponentInChildren<RectTransform>();
+        _gameObject.transform.localScale = new Vector3(0, 0, 0);
     }
 
     private void Update()
@@ -19,13 +21,49 @@ public class ItemPointer : MonoBehaviour
     public void ItemsPointer()
     {
         float distance = Vector3.Distance(transform.position, _player.transform.position);
+        _gameObject.LookAt(_player.transform.position);
 
         if (distance <= 15)
         {
-            _gameObject.gameObject.SetActive(true);
-            _gameObject.LookAt(_player.transform.position);
+            StartCoroutine(MarkerScalePlus());
+            StopCoroutine(MarkerScaleMinus());
         }
         else
+        {
+            StartCoroutine(MarkerScaleMinus());
+            StopCoroutine(MarkerScalePlus());
+        }
+    }
+
+    private IEnumerator MarkerScalePlus()
+    {
+        _gameObject.gameObject.SetActive(true);
+        Vector3 normalScale = new Vector3(0.001f, 0.001f, 0.001f);
+        float scale = 0;
+
+        while (_gameObject.transform.localScale != normalScale)
+        {
+            _gameObject.transform.localScale = new Vector3(scale, scale, scale);
+            scale += 0.0001f;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator MarkerScaleMinus()
+    {
+        Vector3 normalScale = new Vector3(0, 0, 0);
+        float scale = 0.001f;
+
+        while (_gameObject.transform.localScale != normalScale)
+        {
+            _gameObject.transform.localScale = new Vector3(scale, scale, scale);
+            scale -= 0.0001f;
+
+            yield return null;
+        }
+
+        if (scale == 0)
         {
             _gameObject.gameObject.SetActive(false);
         }
