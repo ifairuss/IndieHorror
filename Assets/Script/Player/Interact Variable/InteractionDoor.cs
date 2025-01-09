a09d97715a916a694399729a7bacb17cafe89c8a
ifairuss <ifairuss2403artem@gmail.com>
2025-01-09 18:10:52 +0100

    Feature/Add-Door-Animation(Script)

--- Assets/Script/Player/Interact Variable/InteractionDoor.cs
+++ Assets/Script/Player/Interact Variable/InteractionDoor.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class InteractionDoor : InteractableAbstract
 {
-    [Header("Door Setting")]
+    [Header("Angle Setting")]
+    [SerializeField] private float _openAngle;
+    [SerializeField] private float _closedAngle;
+    [SerializeField] private float _speedAnimation;
     [Space]
     [SerializeField] private bool _isLoockedDoor;
     [SerializeField] private bool _isOpenDoor;
@@ -13,6 +16,25 @@ public class InteractionDoor : InteractableAbstract
     [SerializeField] private string FirstText = "";
     [SerializeField] private string ButtonText = "[E]";
 
+    private void Update()
+    {
+        DoorAnimation();
+    }
+
+    private void DoorAnimation()
+    {
+        Quaternion currentAngle = transform.localRotation;
+
+        if (_isOpenDoor == true)
+        {
+            transform.localRotation = Quaternion.Slerp(currentAngle, Quaternion.Euler(currentAngle.x, _openAngle, currentAngle.z), _speedAnimation * Time.unscaledDeltaTime);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Slerp(currentAngle, Quaternion.Euler(currentAngle.x, _closedAngle, currentAngle.z), _speedAnimation * Time.unscaledDeltaTime);
+        }
+    }
+
     public override void OnFocus()
     {
         InteractionFocus.ActiveButton();
@@ -20,13 +42,16 @@ public class InteractionDoor : InteractableAbstract
     }
     public override void OnInteractable()
     {
+
         if (!_isLoockedDoor)
         {
             if (_isOpenDoor == false)
             {
+                _isOpenDoor = true;
             }
             else
             {
+                _isOpenDoor = false;
             }
         }
         else
